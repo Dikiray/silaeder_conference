342f6f2f61679abfc9a5559f0040fc61c239dcad
Dikiray <dikiray@194-58-90-12.cloudvps.regruhosting.ru>
2022-08-03 11:36:35 +0300

    added api and change project_view

---
+++ api_for_presentations.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI
+import sqlite3
+app = FastAPI()
+
+def remake_data(projects, cur):
+    result = projects
+    for i in range(len(result)):
+        result[i] = {'id': result[i][0], 'name': result[i][1], 'presentation':{'pdf': result[i][2], 'videos': []}}
+        que = "SELECT video_wishes, video FROM silsite_video WHERE id={}".format(result[i]['id'])
+        cur.execute(que)
+        videos = cur.fetchall()
+        for j in videos:
+            result[i]['presentation']['videos'].append({'after_slide': j[0][0], 'YT': j[0][1]})
+    return result
+@app.get("/projects")
+def projects():
+    con = sqlite3.connect('db.sqlite3')
+    cur = con.cursor()
+    que = "SELECT id, name, presentation FROM silsite_project"
+    cur.execute(que)
+    result = cur.fetchall()
+    result = remake_data(result, cur)
+    cur.close()
+    return result
+
+@app.get("/project/{project_id}")
+def project(project_id=None):
+    con = sqlite3.connect('db.sqlite3')
+    cur = con.cursor()
+    que = "SELECT id, name, presentation FROM silsite_project WHERE id={}".format(project_id)
+    cur.execute(que)
+    result = cur.fetchall()
+    result = remake_data(result, cur)
+    cur.close()
+    return result
+@app.get("/project")
+def project_by_name(q=None):
+    con = sqlite3.connect('db.sqlite3')
+    cur = con.cursor()
+    que = "SELECT id, name, presentation FROM silsite_project WHERE name={}".format(q)
+    cur.execute(que)
+    result = cur.fetchall()
+    result = remake_data(result, cur)
+    cur.close()
+    return result
